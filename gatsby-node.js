dbc1ac77dae0c64dd3468e3cdc806a4a60d9766d
gjdickens <geeregg@gmail.com>
2021-11-05 13:38:07 +0200

    merged in author, page and post templates from ghost starter

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,6 +24,23 @@ exports.createPages = async ({ graphql, actions }) => {
                   }
               }
           }
+          allGhostAuthor(sort: { order: ASC, fields: name }) {
+              edges {
+                  node {
+                      slug
+                      url
+                      postCount
+                  }
+              }
+          }
+          allGhostPage(sort: { order: ASC, fields: published_at }) {
+              edges {
+                  node {
+                      slug
+                      url
+                  }
+              }
+          }
       }
     `)
     // Check for any errors
@@ -32,10 +49,14 @@ exports.createPages = async ({ graphql, actions }) => {
     }
     // Extract query results
     const tags = result.data.allGhostTag.edges
+    const authors = result.data.allGhostAuthor.edges
+    const pages = result.data.allGhostPage.edges
     const posts = result.data.allGhostPost.edges
     // Load templates
     const tagsTemplate = path.resolve(`./src/templates/tag.js`)
-    const postTemplate = path.resolve(`./src/templates/blog-post.js`)
+    const authorTemplate = path.resolve(`./src/templates/author.js`)
+    const pageTemplate = path.resolve(`./src/templates/page.js`)
+    const postTemplate = path.resolve(`./src/templates/post.js`)
 
     // Create tag pages
     tags.forEach(({ node }) => {
@@ -58,6 +79,46 @@ exports.createPages = async ({ graphql, actions }) => {
                 slug: node.slug
             }
         })
+    })
+
+    // Create author pages
+    authors.forEach(({ node }) => {
+        const totalPosts = node.postCount !== null ? node.postCount : 0
+
+        // This part here defines, that our author pages will use
+        // a `/author/:slug/` permalink.
+        const url = `/author/${node.slug}`
+
+        const items = Array.from({length: totalPosts})
+
+        // Create pagination
+        paginate({
+            createPage,
+            items: items,
+            itemsPerPage: postsPerPage,
+            component: authorTemplate,
+            pathPrefix: ({ pageNumber }) => (pageNumber === 0) ? url : `${url}/page`,
+            context: {
+                slug: node.slug
+            }
+        })
+    })
+
+    // Create pages
+    pages.forEach(({ node }) => {
+      // This part here defines, that our pages will use
+      // a `/:slug/` permalink.
+      node.url = `/${node.slug}/`
+
+      createPage({
+          path: node.url,
+          component: pageTemplate,
+          context: {
+              // Data passed to context is available
+              // in page queries as GraphQL variables.
+              slug: node.slug,
+          },
+      })
     })
 
     // Create post pages
---
+++ src/templates/page.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
+
+import Layout from '../components/layout'
+
+import { MetaData } from '../components/meta'
+
+/**
+* Single page (/:slug)
+*
+* This file renders a single page and loads all the content.
+*
+*/
+const Page = ({ data, location }) => {
+    const page = data.ghostPage
+    const siteTitle = data.site.siteMetadata?.title || `Title`
+
+    return (
+        <>
+            <MetaData
+                data={data}
+                location={location}
+                type="website"
+            />
+            <Helmet>
+                <style type="text/css">{`${page.codeinjection_styles}`}</style>
+            </Helmet>
+            <Layout location={location} title={siteTitle}>
+                <div className="container">
+                    <article className="content">
+                        <h1 className="content-title">{page.title}</h1>
+
+                        {/* The main page content */}
+                        <section
+                            className="content-body load-external-scripts"
+                            dangerouslySetInnerHTML={{ __html: page.html }}
+                        />
+                    </article>
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+Page.propTypes = {
+    data: PropTypes.shape({
+        ghostPage: PropTypes.shape({
+            codeinjection_styles: PropTypes.object,
+            title: PropTypes.string.isRequired,
+            html: PropTypes.string.isRequired,
+            feature_image: PropTypes.string,
+        }).isRequired,
+    }).isRequired,
+    location: PropTypes.object.isRequired,
+}
+
+export default Page
+
+export const postQuery = graphql`
+    query($slug: String!) {
+        ghostPage(slug: { eq: $slug }) {
+            ...GhostPageFields
+        }
+        site {
+          siteMetadata {
+            title
+          }
+        }
+    }
+`
